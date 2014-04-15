f2eafa32cba74660a11398fa35adc701f71e1be1
Dachi Gogotchuri <dachi.gogotchuri@gmail.com>
2014-04-15 22:33:43 +0200

    Test a Regex pattern in a text
    APRENDE REGEX, EXPRESIONES REGULARES, CON FACILIDAD

---
+++ RegexTester/ViewModels/MainViewModel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using RegexTester.ViewModels.Base;
+
+namespace RegexTester.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string regexPattern;
+
+        private string inputText;
+
+        private string message;
+
+        private Regex regex;
+
+        public MainViewModel()
+        {
+        }
+
+        private void ValidateInputText()
+        {
+            if (regexPattern == String.Empty)
+            {
+                Message = "Regex is Empty";
+                return;
+            }
+
+            try
+            {
+                regex = new Regex(regexPattern);
+                Message = String.Empty;
+            }
+            catch (ArgumentException error)
+            {
+                Message = error.Message;
+                return;
+            }
+
+            if (InputText != null)
+            {
+                bool isMatch = regex.IsMatch(InputText);
+                if (isMatch)
+                {
+                    Message = "It's works!";
+                }
+                else
+                {
+                    Message = "No working :(";
+                }
+            }
+        }
+
+        public string RegexPattern
+        {
+            get { return regexPattern; }
+            set
+            {
+                if (regexPattern != value)
+                {
+                    regexPattern = value;
+                    ValidateInputText();
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string InputText
+        {
+            get { return inputText; }
+            set
+            {
+                if (inputText != value)
+                {
+                    inputText = value;
+                    ValidateInputText();
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Message
+        {
+            get { return message; }
+            set
+            {
+                if (message != value)
+                {
+                    message = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+    }
+}
---
+++ RegexTester/Views/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using RegexTester.ViewModels;
+
+namespace RegexTester.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
